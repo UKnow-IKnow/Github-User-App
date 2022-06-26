5d7be82db6ac27e29adb32e8b7c483c1aeaa1c24
UKnow-IKnow <sarojsahu014@gmail.com>
2022-06-26 10:21:34 +0530

    #2

--- app/src/main/java/com/example/githubapp/ui/details/DetailUserActivity.kt
+++ app/src/main/java/com/example/githubapp/ui/details/DetailUserActivity.kt
@@ -1,21 +1,65 @@
 package com.example.githubapp.ui.details
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.githubapp.R
+import com.example.githubapp.adapters.SectionPagerAdapter
 import com.example.githubapp.databinding.ActivityDetailUserBinding
+import kotlin.time.measureTimedValue
 
 class DetailUserActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val EXTRA_USERNAME = "extra_username"
     }
 
     private lateinit var binding: ActivityDetailUserBinding
 
+    private lateinit var viewModel: DetailUserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(DetailUserViewModel::class.java)
+
+
+        if (username != null) {
+            viewModel.setUseDetail(username)
+        }
+
+        viewModel.getUserDetail().observe(this, {
+            if (it != null) {
+                binding.apply {
+                    tvName.text = it.name
+                    tvUsername.text = it.login
+                    tvFollowers.text = "${it.followers} Followers"
+                    tvFollowing.text = "${it.following} Fpllowing"
+                    Glide.with(this@DetailUserActivity)
+                        .load(it.avatar_url)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                        .centerCrop()
+                        .into(ivProfile)
+                }
+            }
+        })
+
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
+        binding.apply {
+            viewPager.adapter = sectionPagerAdapter
+            tabs.setupWithViewPager(viewPager)
+        }
+
     }
 }
