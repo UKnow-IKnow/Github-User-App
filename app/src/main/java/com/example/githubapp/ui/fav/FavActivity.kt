30d6c992406cbb14a35ea7fe45900e753f0a018c
UKnow-IKnow <sarojsahu014@gmail.com>
2022-07-03 19:37:19 +0530

    small change

--- app/src/main/java/com/example/githubapp/ui/fav/FavActivity.kt
+++ app/src/main/java/com/example/githubapp/ui/fav/FavActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.githubapp.R
 import com.example.githubapp.adapters.UserAdapter
 import com.example.githubapp.data.models.UserResponseItem
 import com.example.githubapp.databinding.ActivityFavBinding
